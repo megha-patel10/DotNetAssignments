246ea798a5e866c58b1f9b914fd6609b43ee88b7
Megha Patel <17.comp.megha.patel@gmail.com>
2020-01-13 13:30:24 +0530

    Practical 1 completed

--- Practicals/Practical1/Program.cs
+++ Practicals/Practical1/Program.cs
@@ -13,6 +13,7 @@ namespace Practical1
             Console.WriteLine("Practical 1");
             pattern1(); // call pattern 1
             pattern2(); // call pattern 2
+            pattern3(); // call pattern 3
         }
         static void pattern1()
         {
@@ -30,6 +31,17 @@ namespace Practical1
             for (int i = 0; i < 5; i++)
             {
                 for (int j = i; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+        static void pattern3()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
                 {
                     Console.Write("*");
                 }
