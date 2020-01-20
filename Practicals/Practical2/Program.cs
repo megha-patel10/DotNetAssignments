97086b625bddea1533ff855048c5b677cd4e54de
Megha Patel <17.comp.megha.patel@gmail.com>
2020-01-20 13:44:30 +0530

    Regex

--- Practicals/Practical2/Program.cs
+++ Practicals/Practical2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Practical2
 {
@@ -12,9 +13,11 @@ namespace Practical2
         {
             string name;
             string country;
-
-            Console.WriteLine("What's your good name?");
-            name = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("What's your good name?");
+                name = Console.ReadLine();
+            } while (!Regex.IsMatch(name, "^[a-zA-Z]+$"));
 
             Console.WriteLine("Where are you from?");
             country = Console.ReadLine();
