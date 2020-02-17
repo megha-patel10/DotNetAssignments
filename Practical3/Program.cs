0150a1ec9e5d4c0e2d13f6da05888457833c0cc5
Megha Patel <17.comp.megha.patel@gmail.com>
2020-02-17 13:46:24 +0530

    Practical 3 complete

---
+++ Practical3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimalToBinary();
+            Console.Read();
+            decimalToHex();
+            Console.Read();
+            decimalToOctal();
+            Console.Read();
+            binaryToDecimal();
+            Console.Read();
+        }
+        static void decimalToBinary()
+        {
+            int x = 16;
+            int reminder, i = 0;
+            int[] binary = new int[100];
+            Console.Write("Decimal To Binary Conversion:");
+            while (x > 0)
+            {
+                reminder = x % 2;
+                binary[i] = reminder;
+                x /= 2;
+                i++;
+            }
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.WriteLine(binary[j]);
+            }
+
+        }
+        static void decimalToOctal()
+        {
+            int x = 16;
+            Console.Write("Decimal To Octal Conversion:");
+            int[] octalNum = new int[100];
+            int i = 0;
+            while (x != 0)
+            {
+
+                octalNum[i] = x % 8;
+                x = x / 8;
+                i++;
+            }
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.WriteLine(octalNum[j]);
+            }
+        }
+        static void decimalToHex()
+        {
+            int x = 50;
+            Console.Write("Decimal To Hex Conversion:");
+            int reminder, i = 0;
+            char[] hex = new char[100];
+            while (x > 0)
+            {
+                reminder = x % 16;
+                if (reminder > 9)
+                {
+                    hex[i] = (char)(reminder + 55);
+                }
+                else
+                {
+                    hex[i] = (char)(reminder + 48);
+                }
+                x /= 16;
+                i++;
+            }
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.WriteLine(hex[j]);
+            }
+
+
+        }
+        public static void binaryToDecimal()
+        {
+            int binaryNumber = 10101;
+            int base1 = 1;
+            int decimalValue = 0;
+            Console.Write("Binary To Decimal Conversion:");
+            while (binaryNumber > 0)
+            {
+                int reminder = binaryNumber % 10;
+                binaryNumber = binaryNumber / 10;
+                decimalValue += reminder * base1;
+
+
+            }
+            Console.WriteLine(decimalValue);
+            Console.Read();
+        }
+
+    }
+
+}
