ac70beb64d72e3e53bbad03b2196974798553503
Megha Patel <17.comp.megha.patel@gmail.com>
2020-02-17 20:04:37 +0530

    Practical 4 finish

--- Practical4/Program.cs
+++ Practical4/Program.cs
@@ -8,8 +8,15 @@ namespace Practical4
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            MyClass myclass = new MyClass();
+            String infix = "(a+b)*(c+d)";
+            String postfix;
+            myclass.Infixtopostfix(ref infix, out postfix);
+            Console.WriteLine("infix string : " + infix);
+            Console.WriteLine("postfix string is : " + postfix);
+            Console.Read();
         }
     }
 }
